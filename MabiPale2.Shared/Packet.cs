3a681b3cae97b056b3a6139708ab0f281a062b3c
exec <exec@dragonbyte.de>
2022-11-20 21:47:15 +0100

    Fixed Packet.GetBin reading length as short

--- MabiPale2.Shared/Packet.cs
+++ MabiPale2.Shared/Packet.cs
@@ -520,7 +520,7 @@ namespace MabiPale2.Shared
 				throw new ArgumentException("Expected Bin, got " + this.Peek() + ".");
 
 			_ptr += 1;
-			var len = IPAddress.NetworkToHostOrder(BitConverter.ToInt16(_buffer, _ptr));
+			var len = (int)(ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(_buffer, _ptr));
 			_ptr += 2;
 
 			var val = new byte[len];
